d724e0f5a88d00a2b635a8f141436e689ef6c100
cs5-sam <54584505+cs5-sam@users.noreply.github.com>
2020-03-28 17:10:45 +0530

    Update MyQueue.java

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -5,7 +5,28 @@
  *  Time: 7:11 PM
  */
 package problem4.myqueue;
+import problem4.linkedList.MyLinkedList;
+import problem4.linkedList.Node;
 // to create queue to store pre - order successor
 
 public class MyQueue {
+  MyLinkedList linkedList = new MyLinkedList();
+    public int peek(int data){
+        if(linkedList.search(data)==null){
+            return -1;
+        }
+        return linkedList.search(data).getData();
+    }
+
+    public void enqueue(int data){
+        linkedList.addLast(data);
+    }
+
+    public void dequeue(){
+        linkedList.deleteFirst();
+    }
+
+    public void show(){
+        linkedList.display();
+    }
 }
